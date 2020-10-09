cd1bc4ca8390153a434d6b8ef2dc65a8620328c9
K Mih <kmihalakakou@gmail.com>
2020-10-09 17:14:20 +0300

    Add files via upload

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.HashMap;
 
 import static java.lang.StrictMath.log;
+import static java.lang.StrictMath.signum;
 
 public class Test {
 
@@ -17,7 +18,7 @@ public class Test {
         int tn=0;
         int spamsFound=0,hamsFound=0;
         int TestSpams=0,TestHams=0;
-
+        int count=0;
         int training = Integer.parseInt(args[1]); //#files to be trained from
         if(training>9||training<1){
             System.out.println("Error!Max num of training files :9 ,min: 1");
@@ -29,73 +30,79 @@ public class Test {
         BufferedReader reader = null;
         String line;
         File f=null;
+        try {
+            f = new File(s); } //the main folder that contains other folders
+        catch (NullPointerException e){
+            System.err.println ("File not found.");
+        }
 
-        for(int i = training+1 ; i <11 ; i++) {//for parts
+        for(File f1: f.listFiles()){//for every testing subfolder
 
-            File f1 = new File(s + "\\" + "part" + i); //part-i,fakelos me texts
-
-            for(File texts: f1.listFiles()){//gia kathe txt
-                if (texts.getName().contains("spmsg")){
-                    bool=true;
-                    TestSpams+=1;
-                }
-                else {
-                    bool = false;
-                    TestHams+=1;
-                }
-                try{
-                    reader = new BufferedReader(new FileReader(texts));
-                }
-                catch (FileNotFoundException e ){
-                    System.err.println("Error opening file!");
-                }
-
-                line = reader.readLine();
-                while(line!=null) {
-                    String[] words = line.split(" ");
-
-                    for (int j = 0; j < words.length; j++) {//mesa se mia grammh
-
-                        if(tr.WordSpamProps.containsKey(words[j])){
-                            totalS = totalS + tr.WordSpamProps.get(words[j]); //pol/se ta P(xi|spam)
-                        }
-                        if(tr.WordHamProps.containsKey(words[j])) {
-                            totalH = totalH + tr.WordHamProps.get(words[j]);//pol/se ta P(xi|ham)
-                        }
+            count++;
+            if (count>training){
+                for(File texts: f1.listFiles()){//gia kathe txt
+                    if (texts.getName().contains("spmsg")){
+                        bool=true;
+                        TestSpams+=1;
                     }
+                    else {
+                        bool = false;
+                        TestHams+=1;
+                    }
+                    try{
+                        reader = new BufferedReader(new FileReader(texts));
+                    }
+                    catch (FileNotFoundException e ){
+                        System.err.println("Error opening file!");
+                    }
+
                     line = reader.readLine();
-                }//end of text
+                    while(line!=null) {
+                        String[] words = line.split(" ");
 
-                probS=log(tr.spam.size())+totalS;//P(spam|xi)=P(spam)*P(xi|spam)/P(xi)
-                probH=log(tr.ham.size())+totalH;//P(ham|xi)=P(ham)*P(xi|ham)/P(xi)
+                        for (int j = 0; j < words.length; j++) {//mesa se mia grammh
 
-                if(probS>probH) {
-                    Spam=true;//to vrike spam
-                    spamsFound++;
-                }
-                else{
-                    hamsFound++;
-                }
+                            if(tr.WordSpamProps.containsKey(words[j])){
+                                totalS = totalS + tr.WordSpamProps.get(words[j]); //pol/se ta P(xi|spam)
+                            }
+                            if(tr.WordHamProps.containsKey(words[j])) {
+                                totalH = totalH + tr.WordHamProps.get(words[j]);//pol/se ta P(xi|ham)
+                            }
+                        }
+                        line = reader.readLine();
+                    }//end of text
 
-                if(Spam&&bool) tp++;//found spam and it is spam
-                if(!Spam&&!bool) tn++;//found ham and it is ham
+                    probS=log((double)tr.numOfSpams/tr.countTrainTxts)+totalS;//P(spam|xi)=P(spam)*P(xi|spam)/P(xi)
+                    probH=log((double)tr.numOfHams/tr.countTrainTxts)+totalH;//P(ham|xi)=P(ham)*P(xi|ham)/P(xi)
+                    if(probS>probH) {
+                        Spam=true;//predicted as Spam
+                        spamsFound++;
+                    }
+                    else{
+                        Spam=false;//predicted as Ham
+                        hamsFound++;
+                    }
 
-                Spam=false;
-                totalH=0;
-                totalS=0;
+                    if(Spam&&bool) tp++;//predicted as Spam and it is Spam
+                    if(!Spam&&!bool) tn++;//predicted as Ham and it is Ham
 
-            }//for texts
+
+                    totalH=0;
+                    totalS=0;
+
+                }//for texts
+
+            }//if
+
         }//for test files
+
         double Recall=(double)tp/TestSpams;//actual spams found/actual spams exists
         double Percision=(double)tp/spamsFound;//actual spams found/spams found
         double f1=2*Recall*Percision/(Recall+Percision);
-        double accuracy=(double)(tp+tn)/(spamsFound+hamsFound);//swsth katataksh/sunolo txts
+        double accuracy=(double)(tp+tn)/(spamsFound+hamsFound);//correct prediction/total predictions
 
-        double anaklisiH=(double)tn/TestHams;
-        double akriviaH=(double)tn/hamsFound;
-		
-        System.out.println("Plithos paradeigmatwn ekpaideusis: "+tr.countTrainTxts);
-        System.out.println("P: "+Percision+" ,R: "+Recall+", F1: "+f1+" , A:"+ accuracy );
+        System.out.println("Number of training messages: "+tr.countTrainTxts);
+        System.out.println("Percision: "+Percision+" ,Recall: "+Recall+", F1: "+f1+" , Accuracy:"+ accuracy );
 
 
     }
--- Train.java
+++ Train.java
@@ -1,18 +1,28 @@
 import java.io.*;
 import java.util.HashMap;
+import java.util.*;
 import java.math.BigDecimal;
 
 import static java.lang.StrictMath.log;
 
 public class Train {
 
-    public static HashMap<String,Integer> ham = new HashMap<String,Integer>();//plithos ham txts pou periexoun thn
-    public static HashMap<String,Integer> spam = new HashMap<String,Integer>();
+    public static Map<String,Integer> ham = new HashMap<String,Integer>();//count ham messages containing the word
+    public static Map<String,Integer> spam = new HashMap<String,Integer>();//count spam messages containing the word
 
-    public static HashMap<String,Integer> multOccuranceH = new HashMap<String,Integer>();
-    public static HashMap<String,Integer> multOccuranceS = new HashMap<String,Integer>();
-    public int numOfSpams=0,numOfHams=0,countWords=0,countTrainTxts=0;
+    public static HashMap<String,Boolean> multOccurance = new HashMap<String,Boolean>();
+    //public static HashMap<String,Boolean> multOccuranceS = new HashMap<String,Boolean>();//DELETE
+    //public Map<String,Boolean> wordCount = new HashMap<String,Boolean>();
+    static HashMap<String,Double> WordHamProps =new HashMap<String, Double>();
+    static HashMap<String, Double> WordSpamProps=new HashMap<String, Double>();
+    public int numOfSpams=0,numOfHams=0,countWords=0,count=0,countTrainTxts=0;
 
+    /**
+     * Reads the files' contents
+     * @param s The name of the main folder e.g.C:\\pu1
+     * @param training The number of subfolders to be used for training
+     * @throws IOException
+     */
     public void ReadFile(String s,int training) throws IOException {
         BufferedReader reader = null;
         String line;
@@ -21,25 +31,24 @@ public class Train {
         boolean bool;
 
         try {
-             f = new File(s); } //fakelos me upofakelous
+             f = new File(s); } //the main folder that contains other folders
         catch (NullPointerException e){
             System.err.println ("File not found.");
         }
 
+        for(File f1: f.listFiles()){//for every subfolder
+            count++;
+            if (count==training+1) break;
 
-        for(int i = 1 ; i <training+1 ; i++){//for parts
-
-            File f1=new File(s + "\\" + "part" + i); //fakelos me texts
-
-            for(File texts: f1.listFiles()){//mesa se ena txt
-                countTrainTxts++;
-                if (texts.getName().contains("spmsg")){
+            for(File texts: f1.listFiles()){//for each message
+                countTrainTxts++;//counting messages//DELETE
+                if (texts.getName().contains("spmsg")){//spam text
                     bool=true;
-                    numOfSpams+=1;
+                    numOfSpams+=1;//counting spam messages
                 }
                 else {
                     bool = false;
-                    numOfHams+=1;
+                    numOfHams+=1;//counting ham messages
                 }
                 try{
                     reader = new BufferedReader(new FileReader(texts));
@@ -50,45 +59,42 @@ public class Train {
 
                 line = reader.readLine();
                 while(line!=null) {
-                    String[] words = line.split(" ");
+                    String[] words = line.split(" ");//words of the line
 
-                    for (int j = 0; j < words.length; j++) {//gia kathe leksi mias garmmis
-                        if(!spam.containsKey(words[j])&&!ham.containsKey(words[j])) countWords++;
+                    if (bool) { //if spam
+                        for (int j = 0; j < words.length; j++) {//for each word of the line
 
-                        if(!(words[j].equals(" ")||words[j].equals("Subject:")||words[j].equals(""))) {
-                            if (bool) { //spamtxtx
-                                if (spam.containsKey(words[j])){
-                                    if(!multOccuranceS.containsKey(words[j])) {
+                            if (!(words[j].equals(" ") || words[j].equals("Subject:") || words[j].equals(""))) {
+                                if (spam.containsKey(words[j])) {
+                                    if (!multOccurance.containsKey(words[j])) {
                                         spam.put(words[j], spam.get(words[j]) + 1);
-                                        multOccuranceS.put(words[j], 1);
+                                        multOccurance.put(words[j],true);
                                     }
+                                } else spam.put(words[j],1);
+                            }
+                        }
+                    } else {//hamtxt
+                        for (int j = 0; j < words.length; j++) {//for each word of the line
 
-                                }
-                                else spam.put(words[j], 1);
-                            } else {//hamtxt
+                            if (!(words[j].equals(" ") || words[j].equals("Subject:") || words[j].equals(""))) {
                                 if (ham.containsKey(words[j])) {
-                                    if(!multOccuranceH.containsKey(words[j])) {
+                                    if (!multOccurance.containsKey(words[j])) {
                                         ham.put(words[j], ham.get(words[j]) + 1);
-                                        multOccuranceH.put(words[j], 1);
+                                        multOccurance.put(words[j],true);
                                     }
-                                }
-                                else ham.put(words[j], 1);
+                                } else ham.put(words[j], 1);
                             }
                         }
                     }
 
                     line = reader.readLine();
                 }//while
-                multOccuranceS.clear();
-                multOccuranceH.clear();
+                multOccurance.clear();//DELETE
+                //multOccuranceH.clear();//DELETE
             }//for inside txts
-
-
         }//for
     }
 
-    static HashMap<String,Double> WordHamProps =new HashMap<String, Double>();
-    static HashMap<String, Double> WordSpamProps=new HashMap<String, Double>();
 
     public void Props(String s,int training) throws IOException {
 
@@ -99,19 +105,21 @@ public class Train {
 
         for (String i : spam.keySet()) {// i = word,get(i) = number
 
-            WordSpam= log((double)(spam.get(i)+1)/(spam.size()+countWords));//P(xi|spam)
+            WordSpam= log((double)(spam.get(i)+1)/(numOfSpams+2));//P(xi|spam)
+
             WordSpamProps.put(i,WordSpam);
             if (!ham.containsKey(i)){
-                WordHam = log((double)(0+1)/(ham.size()+countWords));//P(xi|ham)
+                WordHam = log((double)(0+1)/(numOfHams+2));//P(xi|ham)
                 WordHamProps.put(i,WordHam);
             }
         }
         for (String i : ham.keySet()) {// i = word,get(i) = number
 
-            WordHam = log((double)(ham.get(i)+1)/(ham.size()+countWords));//P(xi|ham)
+            WordHam = log((double)(ham.get(i)+1)/(numOfHams+2));//P(xi|ham) !use log for probabilities getting very small
+
             WordHamProps.put(i,WordHam);
             if(!spam.containsKey(i)){
-                WordSpam= log((double)(0+1)/(spam.size()+countWords));//P(xi|spam)
+                WordSpam= log((double)(0+1)/(numOfSpams+2));//P(xi|spam)
                 WordSpamProps.put(i,WordSpam);
             }
         }
